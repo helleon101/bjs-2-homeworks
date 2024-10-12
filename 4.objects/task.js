81759e56f2a69747486c7e53cce0df829129d6b2
Helleon <nikita.kuklin.01@mail.ru>
2024-10-12 17:04:17 +0300

    Четвертное задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+  this.subject = undefined;
+  this.excluded = undefined;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  if (this.excluded === undefined) {
+    this.subject = subjectName;
+  } else {
+    console.log(
+      `Студент ${this.name} отчислен, установка предмета невозможна.`
+    );
+  }
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks !== undefined) {
+    this.marks.push(...marksToAdd);
+  } else {
+    console.log(`Студент ${this.name} отчислен, добавление оценок невозможно.`);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if (this.marks === undefined || this.marks.length === 0) {
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  return sum / this.marks.length;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  this.subject = undefined;
+  this.marks = undefined;
+  this.excluded = reason;
+};
